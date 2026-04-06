d22b6d570f2c1fe0f443541c57d7360ddf0b9a0b
Mauricio Garrido <mauri101100@gmail.com>
2026-04-06 16:46:35 -0400

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       />
       <Route
         path="*"
-        element={<Navigate to={isAuth ? "/tasks" : "/login"} />}
+        element={<Navigate to="/login" />}
       />
     </Routes>
   );
